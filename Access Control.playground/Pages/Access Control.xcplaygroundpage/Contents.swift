24ef3f9fa007aecfc82dc403007e63230a7687cd
Matthew Harding <matthewpharding@gmail.com>
2023-09-28 18:00:58 +0100

    Added an improved header to the playground

--- Access Control.playground/Pages/Access Control.xcplaygroundpage/Contents.swift
+++ Access Control.playground/Pages/Access Control.xcplaygroundpage/Contents.swift
@@ -1,6 +1,7 @@
 //: ![Swift](swift-logo.png)
-//: Swift v5.7 [official docs](https://docs.swift.org/swift-book/LanguageGuide/AccessControl.html)
-//: # 27. Access Control
+//: Swift v5.7 | [Swift.org](https://docs.swift.org) | [The official Swift Language Guide](https://docs.swift.org/swift-book/documentation/the-swift-programming-language/thebasics)
+//:
+//: # Page 27. Access Control
 //:
 //:
 //: Access control restricts access to parts of your code from code in other source files and modules. This feature enables you to hide the implementation details of your code, and to specify a preferred interface through which that code can be accessed and used.
